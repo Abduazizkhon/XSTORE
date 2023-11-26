d9bb3ca107cae5d57ae7de04a5fc22432365adea
Abdu <abduazizkhon.09@gmail.com>
2023-11-26 12:36:48 +0100

    THird Commit

--- core/settings.py
+++ core/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-)%+!5@11!b6)5optgl47%wjl=v6gci++n5ny*bvlp)(&zriy#&"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["91.147.93.249"]
 
 
 # Application definition
